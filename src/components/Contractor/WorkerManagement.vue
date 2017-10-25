127fffbcdd58bce095f94e5c23f2e1ecedd55f01
MinimiseNZ <32212993+MinimiseNZ@users.noreply.github.com>
2017-10-25 16:13:15 +1300

    Worker Management design complete

--- src/components/Contractor/WorkerManagement.vue
+++ src/components/Contractor/WorkerManagement.vue
@@ -1,6 +1,46 @@
 <template>
   <b-container fluid>
-    <b-card header="Worker Management" header-tag="header">
+    <b-card>
+      <header class="card-header">Worker Management
+       <b-button class="addBtn">Add New Worker</b-button>
+      </header>
+        <b-card
+             v-for="worker in workers"
+             :key="worker.name"
+             class="workerCard mt-2 mb-4" 
+            >
+            <header class="card-header worker">{{worker.name}}
+               <b-button class="editBtn pt-1 pb-1">Edit Worker</b-button>
+            </header>
+              <b-row>
+                <b-col>
+                  <header class="subheader">Personal Information</header>
+                  <ul>
+                    <li>Role: {{worker.role}}</li>
+                    <li>Phone: {{worker.phone}}</li>
+                    <li>Email: {{worker.email}}</li>
+                  </ul>
+                </b-col>
+                <b-col>
+                  <header class="subheader">Training Description</header>
+                  <ul>
+                    <li v-for="training in worker.training">{{training.description}}</li>
+                  </ul>
+                </b-col>
+                <b-col> 
+                  <header class="subheader">Trained Date</header>
+                  <ul>
+                    <li v-for="training in worker.training">{{training.date}}</li>
+                  </ul>
+                </b-col>
+                <b-col>
+                  <header class="subheader">Expiry Date</header>
+                  <ul>
+                    <li v-for="training in worker.training">{{training.expiry}}</li>
+                  </ul>
+                </b-col>
+              </b-row>
+          </b-card>
     </b-card>
   </b-container>
 </template>
@@ -9,6 +49,54 @@
 export default {
   data () {
     return {
+      workers: [
+        {
+          name: 'John Smith',
+          role: 'Project Manager',
+          phone: '021 123 456',
+          email: 'john.smith@stonewoodhomes.co.nz',
+          training: [
+            {
+              description: 'Company Induction',
+              date: '03/04/2017',
+              expiry: '03/04/2018'
+            },
+            {
+              description: 'Site Safe Passport',
+              date: '15/05/2017',
+              expiry: '15/05/2018'
+            },
+            {
+              description: 'First Aid Certrificate',
+              date: '12/04/2017',
+              expiry: '12/04/2018'
+            }
+          ]
+        },
+        {
+          name: 'Sam Stevens',
+          role: 'Worker',
+          phone: '021 111 222',
+          email: 'sam.stevens@stonewoodhomes.co.nz',
+          training: [
+            {
+              description: 'Company Induction',
+              date: '03/04/2017',
+              expiry: '03/04/2018'
+            },
+            {
+              description: 'Site Safe Passport',
+              date: '15/05/2017',
+              expiry: '15/05/2018'
+            },
+            {
+              description: 'First Aid Certrificate',
+              date: '12/04/2017',
+              expiry: '12/04/2018'
+            }
+          ]
+        }
+      ]
     }
   }
 }
@@ -25,8 +113,56 @@ export default {
   }
   
   .card-header {
-    background-color: #335392;
+    background-color: rgba(40, 26, 101, 0.8);
     font-size: 1.4rem;
     color: white;
   }
+  
+  .addBtn {
+    float: right;
+    background-color: #ffc80b;
+    color: black;
+    cursor: pointer;
+  }
+  
+  .editBtn {
+    float: right;
+    background-color:rgba(255, 255, 255, 0.74);
+    color: black;
+   
+  }
+  
+  .card-header.worker{
+    background-color: rgba(69, 59, 111, 0.83);
+    color: white;
+    font-size: 1.2rem;
+    padding-left: 15px;
+  }
+  
+  .subheader {
+    padding: 15px 0 15px 15px;
+    border-bottom: 1px solid lightgrey;
+    font-weight: bold;
+    color: black;
+  }
+  
+  .workerCard > .card-body {
+    padding: 0;
+  }
+  
+  .row {
+    margin:auto;
+  }
+  
+  .col {
+    padding: 0;
+    min-width: 250px;
+  }
+  
+  ul {
+    list-style: none;
+    padding-left: 15px;
+    margin-top: 10px;
+  }
+  
 </style>
